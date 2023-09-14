bfa0be6b21e8a3c0fd725a18976992f4c87abf66
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-09-13 22:56:54 -0500

    Update module-info.java

--- workspace/agenciaViajes/src/main/java/module-info.java
+++ workspace/agenciaViajes/src/main/java/module-info.java
@@ -3,7 +3,11 @@ module co.edu.uniquindio.agenciaViajes {
 	requires javafx.fxml;
 	requires java.persistence;
 	requires lombok;
+	requires javafx.graphics;
 
 	opens co.edu.uniquindio.agenciaViajes.application to javafx.fxml;
+	opens co.edu.uniquindio.agenciaViajes.controllers to javafx.fxml;
+
+	exports co.edu.uniquindio.agenciaViajes.controllers to javafx.fxml;
 	exports co.edu.uniquindio.agenciaViajes.application;
 }
