4b7cbaba9b58bfda26d6b036502dc6fc15aa9bc9
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-09-24 22:18:22 -0500

    Update Application class

--- workspace/agenciaViajes/src/main/java/co/edu/uniquindio/agenciaViajes/application/App.java
+++ workspace/agenciaViajes/src/main/java/co/edu/uniquindio/agenciaViajes/application/App.java
@@ -1,38 +1,37 @@
 package co.edu.uniquindio.agenciaViajes.application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("hola"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	private static Scene scene;
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("main"), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		return new FXMLLoader(App.class.getResource("/co/edu/uniquindio/agenciaviajes/fxml/" + fxml + ".fxml")).load();
+	}
 
 }
