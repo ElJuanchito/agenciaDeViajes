9910027a26079967fe869be74e1f461f82ac6cc5
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2023-10-10 09:30:26 -0500

    Se creo la ventana login
    Se creo la ventana login junto con su controller.

---
+++ workspace/agenciaViajes/src/main/java/co/edu/uniquindio/agenciaViajes/controllers/loginController.java
@@ -0,0 +1,77 @@
+package co.edu.uniquindio.agenciaViajes.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class loginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView pikachuImg;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblBienvenido;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Button btnIniciar;
+
+    @FXML
+    private Label lblNoRegistrado;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    void backEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciarEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void passwordEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrarEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usuarioEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
