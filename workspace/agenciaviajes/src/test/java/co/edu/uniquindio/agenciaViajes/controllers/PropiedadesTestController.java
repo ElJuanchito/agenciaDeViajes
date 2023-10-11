00ef85626fe82a226217f4414d4e66f8cb51eb2c
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-10-11 00:10:05 -0500

    Add a test for button animation

--- workspace/agenciaviajes/src/test/java/co/edu/uniquindio/agenciaViajes/controllers/PropiedadesTestController.java
+++ workspace/agenciaviajes/src/test/java/co/edu/uniquindio/agenciaViajes/controllers/PropiedadesTestController.java
@@ -4,12 +4,14 @@ import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import co.edu.uniquindio.agenciaViajes.services.AnimationService;
 import co.edu.uniquindio.agenciaViajes.services.Propiedades;
+import co.edu.uniquindio.agenciaViajes.services.UtilsJPA;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PropiedadesTestController implements Initializable {
 
@@ -34,6 +36,9 @@ public class PropiedadesTestController implements Initializable {
 	@FXML
 	private Button englishBtn;
 
+	@FXML
+	Button btnInciarSesion;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.location = location;
@@ -56,5 +61,17 @@ public class PropiedadesTestController implements Initializable {
 	@FXML
 	void changeToEsAction(ActionEvent event) {
 		Propiedades.getInstance().setLanguage("es");
+	}
+
+	@FXML
+	public void iniciarSesionAction(ActionEvent event) {
+		AnimationService.getInstance().ejecutarAccionBtn(btnInciarSesion, () -> {
+			try {
+				UtilsJPA.getEntityManager().contains(null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
 	}
 }
