90781359b9358bc5eef6d6a2a9f534b311b39988
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-10-14 12:56:23 -0500

    Rename LoginController

--- workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaViajes/controllers/LoginController.java
+++ workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaViajes/controllers/LoginController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class Login {
+public class LoginController {
 
     @FXML
     private ResourceBundle resources;
