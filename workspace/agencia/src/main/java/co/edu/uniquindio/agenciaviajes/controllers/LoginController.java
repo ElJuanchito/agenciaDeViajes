ec2390e8acd27bb2190fa8369413d3b8e8ae6f99
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-10-23 21:15:22 -0500

    Rename project

---
+++ workspace/agencia/src/main/java/co/edu/uniquindio/agenciaviajes/controllers/LoginController.java
@@ -0,0 +1,152 @@
+package co.edu.uniquindio.agenciaviajes.controllers;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.agenciaviajes.exceptions.FXMLException;
+import co.edu.uniquindio.agenciaviajes.services.AnimationService;
+import co.edu.uniquindio.agenciaviajes.services.DataControllable;
+import co.edu.uniquindio.agenciaviajes.ui.TipoVista;
+import co.edu.uniquindio.agenciaviajes.ui.VistaManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class LoginController implements DataControllable<String> {
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+	@FXML
+	private Button btnBack;
+
+	@FXML
+	private Button btnIniciar;
+
+	@FXML
+	private Button btnRegistrar;
+
+	@FXML
+	private Label lblInfo;
+
+	@FXML
+	private Label lblNoRegistrado;
+
+	@FXML
+	private Label lblTitle;
+
+	@FXML
+	private HBox root;
+
+	@FXML
+	private StackPane stackImg;
+
+	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private PasswordField txtPassword;
+
+	private File[] files;
+
+	private List<File> songs;
+
+	private Media media;
+
+	private MediaPlayer mediaPlayer;
+
+	@FXML
+	void backEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void iniciarEvent(ActionEvent event) {
+		iniciarAction();
+	}
+
+	private void iniciarAction() {
+		AnimationService.getInstance().ejecutarAccionBtn(btnIniciar, () -> {
+			try {
+				VistaManager.getInstance().cambiarVista(TipoVista.DESTINOS, null);
+			} catch (FXMLException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@FXML
+	void registrarEvent(ActionEvent event) {
+		AnimationService.getInstance().ejecutarAccionBtn(btnIniciar, () -> {
+			try {
+				VistaManager.getInstance().cambiarVista(TipoVista.REGISTRO, txtEmail.getText());
+			} catch (FXMLException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@Override
+	public void preInicializar() {
+//		width: 540.0 
+//		height: 720.0
+//		relacionAspecto: 3:4 || 0.75
+		stackImg.prefWidthProperty().bind(root.heightProperty().multiply(0.75d));
+		DJPerdomo();
+	}
+
+	@Override
+	public void updateLanguage(ResourceBundle bundle) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clearData() {
+		txtEmail.clear();
+		txtPassword.clear();
+	}
+
+	@Override
+	public void inicializarDatos(String dato) {
+		if (dato == null)
+			clearData();
+		else {
+			txtEmail.setText(dato);
+		}
+	}
+
+	private void DJPerdomo() {
+
+		files = new File("src/main/resources/co/edu/uniquindio/agenciaViajes/media/songs").listFiles();
+
+		songs = Arrays.asList(files);
+
+		Random random = new Random();
+		int randomIndex = random.nextInt(songs.size());
+
+		media = new Media(songs.get(randomIndex).toURI().toString());
+
+		mediaPlayer = new MediaPlayer(media);
+
+		mediaPlayer.play();
+
+		new Alert(AlertType.CONFIRMATION, "Entre el cielo y la tierra soy el unico Dios").show();
+	}
+
+}
---
+++ workspace/agencia/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+open module co.edu.uniquindio.agenciaViajes {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.persistence;
+	requires lombok;
+	requires java.sql;
+	requires org.hibernate.orm.core;
+	requires transitive javafx.graphics;
+	requires java.desktop;
+	requires javafx.swing;
+	requires javafx.media;
+	
+	exports co.edu.uniquindio.agenciaviajes.model;
+	exports co.edu.uniquindio.agenciaviajes.application;
+}
