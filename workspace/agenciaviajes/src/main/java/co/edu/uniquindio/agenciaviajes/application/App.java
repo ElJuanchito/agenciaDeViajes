60727a7dee7622293b60bbaa22591f5daada00db
ElJuanchito <perdomocardenas18@gmail.com>
2023-10-25 19:42:48 -0500

    Agregando una de las muchas ventanas que faltan

--- workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/application/App.java
+++ workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/application/App.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.agenciaviajes.application;
 import co.edu.uniquindio.agenciaviajes.ui.TipoVista;
 import co.edu.uniquindio.agenciaviajes.ui.VistaManager;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -22,14 +23,16 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		scene = new Scene(new BorderPane());
-		stage.setScene(scene);
-		VistaManager.getInstance().cambiarVista(TipoVista.LOGIN, null);
+//		scene = new Scene(new BorderPane());
+//		stage.setScene(scene);
+//		VistaManager.getInstance().cambiarVista(TipoVista.LOGIN, null);
 		Image icon = new Image(getClass().getResourceAsStream("/co/edu/uniquindio/agenciaviajes/imagenes/login.png"));
-		stage.setTitle("PokeViajes");
-		stage.heightProperty().addListener((obser, oldV, newV) -> stage.setMinWidth((Double) newV * 1.3));
-		stage.setMinHeight(380);
-		stage.centerOnScreen();
+//		stage.setTitle("PokeViajes");
+//		stage.heightProperty().addListener((obser, oldV, newV) -> stage.setMinWidth((Double) newV * 1.3));
+//		stage.setMinHeight(380);
+//		stage.centerOnScreen();
+		scene = new Scene(new FXMLLoader(App.class.getResource("/co/edu/uniquindio/agenciaviajes/fxml/registroGuia.fxml")).load());
+		stage.setScene(scene);
 		stage.getIcons().add(icon);
 		stage.show();
 	}
