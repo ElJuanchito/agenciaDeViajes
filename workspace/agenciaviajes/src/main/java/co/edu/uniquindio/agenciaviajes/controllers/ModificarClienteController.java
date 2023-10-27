f5ec7dafa4e408368d04b3c682d313a2cefecb1a
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2023-10-27 11:38:13 -0500

    Creacion de ventanas.
    - Se creo la ventana de Gestionar las solicitudes de los guias.
    - Se creo la ventana para que el cliente modifique su info
    - Se crearon los controladores de las ventenas creadas
    - Se agregaron 3 imagenes mas de pikachu
    - Se modifico el css de las tablas
    - Se reformo los css de los fondos (Se unieron todos en un solo css)
    - Se agg una clase al style de botones

---
+++ workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/controllers/ModificarClienteController.java
@@ -0,0 +1,72 @@
+package co.edu.uniquindio.agenciaviajes.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class ModificarClienteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private HBox root;
+
+    @FXML
+    private StackPane stackImg;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblInfo;
+
+    @FXML
+    private TextField txtIdentificacion;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    void actualizarEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
