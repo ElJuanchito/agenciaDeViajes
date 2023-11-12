4a4beccb61e52d2d60f6aad0f72e3cef1e7746d4
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-11-11 22:05:16 -0500

    Add logic controller

---
+++ workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/controllers/CrearReservaLogicController.java
@@ -0,0 +1,97 @@
+package co.edu.uniquindio.agenciaviajes.controllers;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import co.edu.uniquindio.agenciaviajes.exceptions.FXMLException;
+import co.edu.uniquindio.agenciaviajes.exceptions.PeticionException;
+import co.edu.uniquindio.agenciaviajes.model.Cliente;
+import co.edu.uniquindio.agenciaviajes.model.GuiaTuristico;
+import co.edu.uniquindio.agenciaviajes.model.Paquete;
+import co.edu.uniquindio.agenciaviajes.model.Reserva;
+import co.edu.uniquindio.agenciaviajes.viewcontrollers.MainPaneController;
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.util.Callback;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CrearReservaLogicController {
+	private static CrearReservaLogicController instance;
+	private DateTimeFormatter pattern;
+
+	public static CrearReservaLogicController getInstance() {
+		if (instance == null)
+			instance = new CrearReservaLogicController();
+		return instance;
+	}
+
+	public void crearReserva(Integer cantPersonas, Paquete paquete, GuiaTuristico guiaTuristico) {
+
+		Reserva reserva = Reserva.builder().cliente((Cliente) DataController.getInstance().getLoginActual())
+				.cantPersonas(cantPersonas).paquete(paquete).guiaTuristico(guiaTuristico).build();
+		try {
+			Reserva reservaNueva = new PeticionController<Reserva, Reserva>(TipoPeticion.AGREGAR, reserva)
+					.realizarPeticion();
+			MainPaneController.getInstance()
+					.showAlertAccept("Se ha creado una reserva, ¿quieres recibir un comprobante?", () -> {
+						Platform.runLater(() -> crearPdfReserva(reservaNueva));
+					});
+		} catch (PeticionException e) {
+			MainPaneController.getInstance().showAlert(e.getMessage());
+		}
+	}
+
+	private void crearPdfReserva(final Reserva reservaNueva) {
+		try {
+			VistaManager.getInstance().crearPDFReserva(reservaNueva);
+		} catch (FXMLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public IntegerSpinnerValueFactory crearSpinnerValueFactory(int cupoMax) {
+		return new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, 0) {
+			@Override
+			public void decrement(int steps) {
+				int currentValue = this.getValue();
+				int newValue = currentValue - steps;
+				this.setValue(newValue >= 0 ? newValue : 0);
+			}
+
+			@Override
+			public void increment(int steps) {
+				if (this.getValue() + steps <= cupoMax)
+					this.setValue(this.getValue() + steps);
+			}
+		};
+	}
+
+	private ReadOnlyStringWrapper getWraper(String s) {
+		return new ReadOnlyStringWrapper(s);
+	}
+
+	public Callback<CellDataFeatures<GuiaTuristico, String>, ObservableValue<String>> crearColID() {
+		return cell -> getWraper(cell.getValue().getIdentificacion());
+	}
+
+	public Callback<CellDataFeatures<GuiaTuristico, String>, ObservableValue<String>> crearColNombre() {
+		return cell -> getWraper(cell.getValue().getNombreCompleto());
+	}
+
+	public Callback<CellDataFeatures<GuiaTuristico, String>, ObservableValue<String>> crearColExperiencia() {
+		return cell -> getWraper(new DecimalFormat("#").format(cell.getValue().getExpHoras()));
+	}
+
+	public String getFechaFormato(LocalDateTime fecha) {
+		if (pattern == null)
+			pattern = DateTimeFormatter.ofPattern("dd/MM/yyyy h:mm a");
+		return fecha.format(pattern);
+	}
+}
--- workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/viewcontrollers/CrearReservaController.java
+++ workspace/agenciaviajes/src/main/java/co/edu/uniquindio/agenciaviajes/viewcontrollers/CrearReservaController.java
@@ -1,23 +1,12 @@
 package co.edu.uniquindio.agenciaviajes.viewcontrollers;
 
-import java.text.DecimalFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
-import co.edu.uniquindio.agenciaviajes.controllers.DataController;
-import co.edu.uniquindio.agenciaviajes.controllers.PeticionController;
-import co.edu.uniquindio.agenciaviajes.controllers.TipoPeticion;
-import co.edu.uniquindio.agenciaviajes.controllers.VistaManager;
-import co.edu.uniquindio.agenciaviajes.exceptions.FXMLException;
-import co.edu.uniquindio.agenciaviajes.exceptions.PeticionException;
-import co.edu.uniquindio.agenciaviajes.model.Cliente;
+import co.edu.uniquindio.agenciaviajes.controllers.CrearReservaLogicController;
 import co.edu.uniquindio.agenciaviajes.model.GuiaTuristico;
 import co.edu.uniquindio.agenciaviajes.model.Idioma;
 import co.edu.uniquindio.agenciaviajes.model.Paquete;
-import co.edu.uniquindio.agenciaviajes.model.Reserva;
 import co.edu.uniquindio.agenciaviajes.services.DataControllable;
-import javafx.application.Platform;
-import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,8 +14,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
-import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -54,8 +41,6 @@ public class CrearReservaController implements DataControllable<Paquete> {
 	@FXML
 	private TableView<Idioma> tblIdiomas;
 
-	private DateTimeFormatter dateTimePattern;
-
 	private Paquete paquete;
 
 	@FXML
@@ -70,15 +55,12 @@ public class CrearReservaController implements DataControllable<Paquete> {
 
 	@Override
 	public void preInicializar() {
+	
+		CrearReservaLogicController instance = CrearReservaLogicController.getInstance();
 
-		dateTimePattern = DateTimeFormatter.ofPattern("dd/MM/yyyy h:mm a");
-
-		clmId.setCellValueFactory(cell -> new ReadOnlyStringWrapper(cell.getValue().getIdentificacion()));
-		clmName.setCellValueFactory(cell -> new ReadOnlyStringWrapper(cell.getValue().getNombreCompleto()));
-
-		DecimalFormat format = new DecimalFormat("#");
-
-		clmExp.setCellValueFactory(cell -> new ReadOnlyStringWrapper(format.format(cell.getValue().getExpHoras())));
+		clmId.setCellValueFactory(instance.crearColID());
+		clmName.setCellValueFactory(instance.crearColNombre());
+		clmExp.setCellValueFactory(instance.crearColExperiencia());
 
 		// Actualizacion de tablas por cada vez que se seleccione un guia
 		tblGuias.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
@@ -89,24 +71,8 @@ public class CrearReservaController implements DataControllable<Paquete> {
 		});
 
 		// Configurar el Spinner con el SpinnerValueFactory personalizado
-		spnCant.setValueFactory(crearSpinnerValueFactory());
+		spnCant.setValueFactory(instance.crearSpinnerValueFactory(Integer.MAX_VALUE));
 
-	}
-
-	private IntegerSpinnerValueFactory crearSpinnerValueFactory() {
-		return new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, 0) {
-			@Override
-			public void decrement(int steps) {
-				int currentValue = this.getValue();
-				int newValue = currentValue - steps;
-				this.setValue(newValue >= 0 ? newValue : 0);
-			}
-
-			@Override
-			public void increment(int steps) {
-				this.setValue(this.getValue() + steps);
-			}
-		};
 	}
 
 	private void limpiarSeleccionAction() {
@@ -117,7 +83,6 @@ public class CrearReservaController implements DataControllable<Paquete> {
 	@Override
 	public void updateLanguage(ResourceBundle bundle) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -129,37 +94,19 @@ public class CrearReservaController implements DataControllable<Paquete> {
 
 	@Override
 	public void inicializarDatos(Paquete paquete) {
+		CrearReservaLogicController instance = CrearReservaLogicController.getInstance();
 		this.paquete = paquete;
 		lblNamePackage.setText(paquete.getNombre());
-		lblInfoFechaInicial.setText(paquete.getFechaIncio().format(dateTimePattern));
-		lblInfoFechaFinal.setText(paquete.getFechaFin().format(dateTimePattern));
+
+		lblInfoFechaInicial.setText(instance.getFechaFormato(paquete.getFechaIncio()));
+		lblInfoFechaFinal.setText(instance.getFechaFormato(paquete.getFechaFin()));
 		lblInfoCupoMaximo.setText(String.valueOf(paquete.getCupoMaximo()));
+		spnCant.setValueFactory(instance.crearSpinnerValueFactory(paquete.getCupoMaximo()));
 	}
 
 	private void reservarAction() {
-
-		Reserva reserva = Reserva.builder().cliente((Cliente) DataController.getInstance().getLoginActual())
-				.cantPersonas(spnCant.getValue()).paquete(paquete)
-				.guiaTuristico(tblGuias.getSelectionModel().getSelectedItem()).build();
-		try {
-			Reserva reservaNueva = new PeticionController<Reserva, Reserva>(TipoPeticion.AGREGAR, reserva)
-					.realizarPeticion();
-			MainPaneController.getInstance()
-					.showAlertAccept("Se ha creado una reserva, ¿quieres recibir un comprobante?", () -> {
-						Platform.runLater(() -> crearPdfReserva(reservaNueva));
-					});
-		} catch (PeticionException e) {
-			MainPaneController.getInstance().showAlert(e.getMessage());
-		}
-
-	}
-
-	private void crearPdfReserva(final Reserva reservaNueva) {
-		try {
-			VistaManager.getInstance().crearPDFReserva(reservaNueva);
-		} catch (FXMLException e) {
-			throw new RuntimeException(e);
-		}
+		CrearReservaLogicController.getInstance().crearReserva(spnCant.getValue(), paquete,
+				tblGuias.getSelectionModel().getSelectedItem());
 	}
 
 }
